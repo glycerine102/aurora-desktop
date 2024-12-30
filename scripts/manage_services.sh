4166f14771bad8dd2bd1a41f7a8a30e84ef50b01
Nathan Dorsey <421420+glycerine102@users.noreply.github.com>
2024-12-30 13:40:56 -0500

    Ensure sshd is enabled and add auto mounts

--- scripts/manage_services.sh
+++ scripts/manage_services.sh
@@ -5,6 +5,14 @@ set -ouex pipefail
 # Enable Services
 systemctl enable podman.socket
 systemctl enable input-remapper.service
+systemctl enable sshd.service
 
 # Disable Services
 systemctl disable tailscaled.service
+
+# Enable Mounts
+systemctl enable var-mnt-tuxbox-learning.automount
+systemctl enable var-mnt-tuxbox-movies.automount
+systemctl enable var-mnt-tuxbox-music.automount
+systemctl enable var-mnt-tuxbox-photos.automount
+systemctl enable var-mnt-tuxbox-television.automount
