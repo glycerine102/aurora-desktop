c61bc35652659906f2c5c51a5b9c25377cb3bbbf
Nathan Dorsey <421420+glycerine102@users.noreply.github.com>
2024-12-30 13:13:15 -0500

    Removes akonadi and mariadb packages from the base

--- scripts/remove_packages.sh
+++ scripts/remove_packages.sh
@@ -8,6 +8,15 @@ system_packages=(
   "docker-buildx-plugin"
   "docker-compose-plugin"
   "docker-ce-rootless-extras"
+  "akonadi-server"
+  "akonadi-server-mysql"
+  "mariadb"
+  "mariadb-backup"
+  "mariadb-common"
+  "mariadb-cracklib-password-check"
+  "mariadb-errmsg"
+  "mariadb-gssapi-server"
+  "mariadb-server"
 )
 
 packages=(
